dab91610d0b1cef7a74e44f273c9dfba0c06e842
Naliwe <r.fourdrilis@gmail.com>
2017-08-25 16:05:52 +0200

    fixed lua

--- aur_utils/scripts/hihi.lua
+++ aur_utils/scripts/hihi.lua
@@ -1,6 +1,16 @@
+if (not counter) then
+    counter = 0
+end
+
+counter = counter + getDeltaFrames()
+
 local object = getEntity("First")
 
 local pos = object:getTransformComponent().boundingBox.topLeft
 
-pos.x = math.random(0, getWindowSize().x / getTileSize().x)
-pos.y = math.random(0, getWindowSize().y / getTileSize().y)
+if (counter > 30) then
+    pos.x = math.random(0, getWindowSize().x / getTileSize().x)
+    pos.y = math.random(0, getWindowSize().y / getTileSize().y)
+    counter = 0
+end
+
